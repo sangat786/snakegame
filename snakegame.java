551b058cf4cf00db6c599cb33855ae2d82bfd67f
sangat786 <137291664+sangat786@users.noreply.github.com>
2023-06-29 22:26:11 +0530

    Add files via upload
    here is the project of snake game

---
+++ snakegame.java
@@ -0,0 +1,17 @@
+import javax.swing.*;
+
+public class snakegame extends JFrame {
+    Board board;
+    snakegame(){
+        board  = new Board();
+        add(board);
+        pack();
+        setResizable(false);
+        setVisible(true);
+
+    }
+    public static void main(String[] args) {
+        snakegame Snakegame = new snakegame();
+    }
+}
+
